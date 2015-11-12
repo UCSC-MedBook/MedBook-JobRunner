4e33f9a2f7e7906b19b8fa154d0540d23f758b4b
mokolodi1 <mokolodi1@gmail.com>
2015-11-12 01:44:01 -0800

    ExportFile for copy number works

--- webapp/server/classes/ExportFile.js
+++ webapp/server/classes/ExportFile.js
@@ -19,7 +19,7 @@ ExportFile.prototype.writeCopyNumberFile = function (filePath) {
     collaborations: {$in: this.exportedFile.collaborations}
   };
 
-  console.log("copyNumberSelector:", copyNumberSelector);
+  console.log("aggregating data:", copyNumberSelector);
   var aggregationResult = CopyNumber.aggregate([
       {$match: copyNumberSelector},
       {
@@ -43,6 +43,7 @@ ExportFile.prototype.writeCopyNumberFile = function (filePath) {
   var geneLabels = aggregationResult.geneLabels;
   sampleLabels.sort();
   geneLabels.sort();
+  console.log("done aggregating data... starting to write");
 
   // write the header line
   writeStream.write('Gene\t');
@@ -52,92 +53,99 @@ ExportFile.prototype.writeCopyNumberFile = function (filePath) {
   });
   writeStream.write('\n');
 
-  var deferred = Q.defer();
-  writeStream.end(deferred.resolve);
-  return deferred.promise;
+  // get data for the rest of the file
+  var fields = { gene: 1 };
+  _.each(this.sampleList, function (value, key) {
+    fields["samples." + key] = value;
+  });
+  var count = 0;
+  var copyNumberCursor = CopyNumber.rawCollection()
+      .find(copyNumberSelector);
 
-  // // get data for the rest of the file
-  // var fields = { gene: 1 };
-  // _.each(this.sampleList, function (value, key) {
-  //   fields["samples." + key] = value;
-  // });
-  // var count = 0;
-  // var copyNumberCursor = CopyNumber.rawCollection()
-  //     .find(copyNumberSelector);
+  // set up helper functions to write the rest of the file
   //
-  // // set up helper functions to write the rest of the file
-  // //
-  // // The reason we need all these functions instead of just calling write a
-  // // bunch of times is because when write returns false, it's good practice
-  // // to wait for the 'drain' event from the stream, meaning the internal
-  // // buffer has been cleared and written to the disk. In practice, not
-  // // waiting for this event and calling write many times means that writing
-  // // becomes incredibly slow.
-  // //
-  // // https://nodejs.org/api/stream.html#stream_event_drain
+  // The reason we need all these functions instead of just calling write a
+  // bunch of times is because when write returns false, it's good practice
+  // to wait for the 'drain' event from the stream, meaning the internal
+  // buffer has been cleared and written to the disk. In practice, not
+  // waiting for this event and calling write many times means that writing
+  // becomes incredibly slow.
   //
-  // // expressionDeferred.resolve will be called when writeNextLine finds
-  // // the end of the expression cursor
-  // var expressionDeferred = Q.defer();
-  //
-  // function niceWrite(toWrite) {
-  //   var keepWriting = writeStream.write(toWrite);
-  //   if (keepWriting) {
-  //     // return a promise that has already been resolved
-  //     // any .then()s connected to this will fire immidiately
-  //     return Q();
-  //   }
-  //
-  //   // waits until the stream has drained, then resolves
-  //   return new Q.Promise(function (resolve) {
-  //     writeStream.once("drain", resolve);
-  //   });
-  // }
-  //
-  // function writeArray(arrayOfStrings) {
-  //   // NOTE: The way I'm starting all of the writes here means there could
-  //   // be multiple 'drain' events on writeStream. This is not a probelm
-  //   // because in this context we're only calling writeArray with less than
-  //   // 10 elements in arrayOfStrings.
-  //
-  //   var arrayPromises = [];
-  //   for (var index in arrayOfStrings) {
-  //     arrayPromises.push(niceWrite(arrayOfStrings[index]));
-  //   }
-  //   return Q.all(arrayPromises);
-  // }
-  //
-  // function writeNextLine() {
-  //   expressionCursor.nextObject(function (error, expressionDoc) {
-  //     // check to see if we've found the end of the cursor
-  //     if (!expressionDoc) {
-  //       writeStream.end(expressionDeferred.resolve);
-  //       return; // don't run the rest of the function
-  //     }
-  //
-  //     // actually write to the file
-  //     var toWriteArray = [];
-  //     toWriteArray.push(expressionDoc.gene);
-  //     toWriteArray.push('\t');
-  //     var sampleArray = []; // don't call write more than we need
-  //     _.map(expressionDoc.samples, function(value, key) {
-  //       if (self.sampleList[key] !== undefined) {
-  //         geneExp = value.rsem_quan_log2;
-  //         sampleArray.push(geneExp);
-  //       }
-  //     });
-  //     toWriteArray.push(sampleArray.join('\t'));
-  //     toWriteArray.push('\n');
-  //
-  //     // write toWriteArray to the file, then write the next line
-  //     writeArray(toWriteArray).then(writeNextLine);
-  //   });
-  // }
-  //
-  // // start out the promise-based recursive looping through the cursor
-  // writeNextLine();
-  //
-  // return expressionDeferred.promise;
+  // https://nodejs.org/api/stream.html#stream_event_drain
+
+  // deferred.resolve will be called when writeNextLine finds
+  // the end of the expression cursor
+  var deferred = Q.defer();
+
+  function niceWrite(toWrite) {
+    var keepWriting = writeStream.write(toWrite);
+    if (keepWriting) {
+      // return a promise that has already been resolved
+      // any .then()s connected to this will fire immidiately
+      return Q();
+    }
+
+    // waits until the stream has drained, then resolves
+    return new Q.Promise(function (resolve) {
+      writeStream.once("drain", resolve);
+    });
+  }
+
+  // // for testing
+  // geneLabels = ['TLR4'];
+  // geneLabels = geneLabels.slice(0, 100);
+
+  function writeArray(arrayOfStrings) {
+    // NOTE: The way I'm starting all of the writes here means there could
+    // be multiple 'drain' events on writeStream. This is not a probelm
+    // because in this context we're only calling writeArray with less than
+    // 10 elements in arrayOfStrings.
+
+    var arrayPromises = [];
+    for (var index in arrayOfStrings) {
+      arrayPromises.push(niceWrite(arrayOfStrings[index]));
+    }
+    return Q.all(arrayPromises);
+  }
+
+  var geneIndex = 0;
+  function writeNextLine() {
+    // check to see if we should stop
+    if (geneIndex >= geneLabels.length) {
+      writeStream.end(deferred.resolve);
+      return; // don't run the rest of the function
+    }
+
+    var gene_label = geneLabels[geneIndex];
+    if (geneIndex % 1000 === 0) {
+      console.log("geneIndex:", geneIndex);
+    }
+    geneIndex++;
+
+    CopyNumber.rawCollection()
+      .find({
+        gene_label: gene_label
+      }, {
+        sample_label: 1,
+        value: 1,
+      })
+      .sort({sample_label: 1})
+      .toArray(function (error, docArray) {
+        var valueArray = _.pluck(docArray, 'value');
+        var toWriteArray = [
+          gene_label,
+          '\t',
+          valueArray.join('\t'),
+          '\n'
+        ];
+        writeArray(toWriteArray).then(writeNextLine);
+      });
+  }
+
+  // start out the promise-based recursive looping through the cursor
+  writeNextLine();
+
+  return deferred.promise;
 };
 ExportFile.prototype.run = function () {
   var self = this;
@@ -150,8 +158,6 @@ ExportFile.prototype.run = function () {
   var deferred = Q.defer();
   this.writeCopyNumberFile.call(this, copyNumberFilePath)
     .then(Meteor.bindEnvironment(function () {
-      console.log("done writing file!");
-
       var blob = Blobs.insert(copyNumberFilePath);
       if (!self.job.user_id) {
         throw "self.job.user_id not set";
@@ -171,7 +177,6 @@ ExportFile.prototype.run = function () {
         }
       });
 
-      console.log("about to resolve");
       deferred.resolve();
     }, deferred.reject));
 
@@ -185,5 +190,14 @@ ExportFile.prototype.onError = function (e) {
     }
   });
 };
+
+// TODO: print if we're actually going to create one
+CopyNumber.rawCollection().ensureIndex({
+  gene_label: 1
+}, function (error, result) {
+  console.log("created index for copy_number");
+  console.log("error:", error);
+  console.log("result:", result);
+});
 
 JobClasses.ExportFile = ExportFile;
