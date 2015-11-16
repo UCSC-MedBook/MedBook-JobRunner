065495fa5bf2c920b856a0fe8c7cc762b881d4b2
mokolodi1 <mokolodi1@gmail.com>
2015-11-16 12:06:06 -0800

    ExportFile: writeFile instead of streams

--- webapp/server/classes/ExportFile.js
+++ webapp/server/classes/ExportFile.js
@@ -13,8 +13,6 @@ ExportFile.prototype.constructor = ExportFile;
 ExportFile.prototype.writeCopyNumberFile = function (filePath) {
   var self = this;
 
-  var writeStream = fs.createWriteStream(filePath);
-
   var copyNumberSelector = {
     collaborations: {$in: this.exportedFile.collaborations}
   };
@@ -45,106 +43,40 @@ ExportFile.prototype.writeCopyNumberFile = function (filePath) {
   geneLabels.sort();
   console.log("done aggregating data... starting to write");
 
-  // write the header line
-  writeStream.write('Gene\t');
-  _.map(sampleLabels, function(value, key) {
-    writeStream.write(value);
-    writeStream.write('\t');
+  // header line
+  var headerLine = 'Gene\t';
+  _.map(sampleLabels, function (value) {
+    headerLine += value + '\t';
   });
-  writeStream.write('\n');
+  headerLine += '\n';
+  var fileArray = [headerLine];
 
-  // get data for the rest of the file
-  var fields = { gene: 1 };
-  _.each(this.sampleList, function (value, key) {
-    fields["samples." + key] = value;
-  });
-  var count = 0;
-  var copyNumberCursor = CopyNumber.rawCollection()
-      .find(copyNumberSelector);
-
-  // set up helper functions to write the rest of the file
-  //
-  // The reason we need all these functions instead of just calling write a
-  // bunch of times is because when write returns false, it's good practice
-  // to wait for the 'drain' event from the stream, meaning the internal
-  // buffer has been cleared and written to the disk. In practice, not
-  // waiting for this event and calling write many times means that writing
-  // becomes incredibly slow.
-  //
-  // https://nodejs.org/api/stream.html#stream_event_drain
-
-  // deferred.resolve will be called when writeNextLine finds
-  // the end of the expression cursor
-  var deferred = Q.defer();
-
-  function niceWrite(toWrite) {
-    var keepWriting = writeStream.write(toWrite);
-    if (keepWriting) {
-      // return a promise that has already been resolved
-      // any .then()s connected to this will fire immidiately
-      return Q();
-    }
-
-    // waits until the stream has drained, then resolves
-    return new Q.Promise(function (resolve) {
-      writeStream.once("drain", resolve);
-    });
-  }
-
-  // // for testing
-  // geneLabels = ['TLR4'];
-  // geneLabels = geneLabels.slice(0, 100);
-
-  function writeArray(arrayOfStrings) {
-    // NOTE: The way I'm starting all of the writes here means there could
-    // be multiple 'drain' events on writeStream. This is not a probelm
-    // because in this context we're only calling writeArray with less than
-    // 10 elements in arrayOfStrings.
-
-    var arrayPromises = [];
-    for (var index in arrayOfStrings) {
-      arrayPromises.push(niceWrite(arrayOfStrings[index]));
-    }
-    return Q.all(arrayPromises);
-  }
-
-  var geneIndex = 0;
-  function writeNextLine() {
-    // check to see if we should stop
-    if (geneIndex >= geneLabels.length) {
-      writeStream.end(deferred.resolve);
-      return; // don't run the rest of the function
-    }
+  // write rest of file
+  for (var geneIndex = 0; geneIndex < geneLabels.length; geneIndex++) {
+    if (geneIndex % 1000 === 0) console.log("geneIndex:", geneIndex);
 
     var gene_label = geneLabels[geneIndex];
-    if (geneIndex % 1000 === 0) {
-      console.log("geneIndex:", geneIndex);
-    }
-    geneIndex++;
 
-    CopyNumber.rawCollection()
-      .find({
+    var docArray = CopyNumber.find({
         gene_label: gene_label
       }, {
-        sample_label: 1,
-        value: 1,
+        fields: { value: 1, sample_label: 1 },
+        sort: { sample_label: 1 }
       })
-      .sort({sample_label: 1})
-      .toArray(function (error, docArray) {
-        var valueArray = _.pluck(docArray, 'value');
-        var toWriteArray = [
-          gene_label,
-          '\t',
-          valueArray.join('\t'),
-          '\n'
-        ];
-        writeArray(toWriteArray).then(writeNextLine);
-      });
+      .fetch();
+
+    var valueArray = _.pluck(docArray, 'value');
+    fileArray.push(gene_label + '\t' + valueArray.join('\t') + '\n');
   }
 
-  // start out the promise-based recursive looping through the cursor
-  writeNextLine();
-
+  var deferred = Q.defer();
+  fs.writeFile(filePath, fileArray.join(','), function (error, result) {
+    if (error) {
+      deferred.reject(error);
+    } else {
+      deferred.resolve(result);
+    }
+  });
   return deferred.promise;
 };
 ExportFile.prototype.run = function () {
@@ -162,6 +94,7 @@ ExportFile.prototype.run = function () {
       if (!self.job.user_id) {
         throw "self.job.user_id not set";
       }
+      
       Blobs.update(blob._id, {
         metadata: {
           user_id: self.job.user_id,
