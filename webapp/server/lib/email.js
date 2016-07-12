eeedb8a40ca80dc4d84edfe49609e7c78acba7e2
Robert Baertsch <baertsch@soe.ucsc.edu>
2016-07-11 20:02:21 -0700

    remove meteor method to send email

--- webapp/server/lib/email.js
+++ webapp/server/lib/email.js
@@ -1,20 +1,4 @@
 Meteor.methods({
-  sendEmail: function (to, subject, text) {
-    check([to, subject, text], [String]);
-
-    // Let other method calls from the same client start running,
-    // without waiting for the email sending to complete.
-    this.unblock();
-    console.log('user' , to, 'notified', subject);
-      Email.send({
-        to: to,
-        from: "info@medbook.ucsc.edu",
-        subject: subject,
-        text: text
-      });
-    }
-});
-Meteor.methods({
   get_email : function(user_id) {
     check(user_id, String);
     var user = "";
