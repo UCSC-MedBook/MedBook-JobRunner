2dc0980650e228662eda0feaa633de7b814413e9
Robert Baertsch <baertsch@soe.ucsc.edu>
2016-07-20 13:24:47 -0700

    add collaborations packages and call function to get members , seems to work. Now to add mysql update

--- webapp/server/classes/UpdateCbioSecurity.js
+++ webapp/server/classes/UpdateCbioSecurity.js
@@ -1,14 +1,19 @@
 function UpdateCbioSecurity (job_id) {
+  console.log('updateCbioSec  urity()')
   Job.call(this, job_id);
 }
 UpdateCbioSecurity.prototype = Object.create(Job.prototype);
 UpdateCbioSecurity.prototype.constructor = UpdateCbioSecurity;
 
 UpdateCbioSecurity.prototype.run = function () {
+  console.log('starting UpdateCbioSecurity')
   for (var i in this.job.args.collaborationList) {
     var c = this.job.args.collaborationList[i]
     console.log('collab list',c, 'index' , i)
-    var members = Collaborations.findOne({name:c}).getAssociatedCollaborators()
+    //Collaborations = new Meteor.Collection("collaborations");
+    var cobj = Collaborations.findOne({name:c})
+    console.log('collab ',cobj)
+    var members = cobj.getAssociatedCollaborators()
     console.log('members',members)
   };
 }
