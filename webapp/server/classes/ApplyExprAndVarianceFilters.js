e0249d46e7bff53d0ffdce5d2ea43c13646f30dd
ETK <e-t-k@users.noreply.github.com>
2016-07-27 16:12:47 -0700

    [WIP/untested] runs expression filter in job
    - adds filters, python to my personal config
    - expression filter is applied
    - VARIANCE FILTER NOT APPLIED YET
    - creates blob2 with the result

--- webapp/server/classes/ApplyExprAndVarianceFilters.js
+++ webapp/server/classes/ApplyExprAndVarianceFilters.js
@@ -7,28 +7,93 @@ ApplyExprAndVarianceFilters.prototype = Object.create(Job.prototype);
 ApplyExprAndVarianceFilters.prototype.constructor = ApplyExprAndVarianceFilters;
 
 ApplyExprAndVarianceFilters.prototype.run = function () {
-  // Set up the working directory
+
   var workDir = ntemp.mkdirSync("ApplyExprAndVarianceFilters");
-  console.log("workDir: ", workDir);
+  console.log("ApplyExprAndVarianceFilters: workDir: ", workDir);
 
-  console.log("Running apply expr job! TODO implement me");
+  var self = this;
+  var sample_group_id = self.job.args.sample_group_id;
+  console.log("using sgid", sample_group_id); // XXX 
 
-// TODO
-//  var deferred = Q.defer();
-//  var self = this;
-//
-//  // export the sample group info
-//  Q.fcall(function(){
-//
-//    // returns "first"
-//  }).then(
-//    // Apply the filters
-//   function(res){}, function(err){} 
-//  ); // TODO
+  var exportScript = getSetting("genomic_expression_export");
+  var python = getSetting("python");
+  var exprFilterScript = getSetting("expression_level_gene_filter");
+  var varianceFilterScript = getSetting("variance_gene_filter");
 
-  // then
-  // re-upload the result as a blob associated with the sample group
-   
+  var deferred = Q.defer();
+
+  // exportCommand is a promise for exporting the
+  // sample group data
+  var exportCommand = spawnCommand(exportScript, [
+      "--sample_group_id", sample_group_id,
+    ], workDir)  
+
+  // Export the data
+  exportCommand.then(function(exportResults){ 
+    console.log("export command ran with results", exportResults);
+    if(exportResults.exitCode !== 0){
+      // TODO print stderr for the command
+      throw new Error("Writing file failed (exit code not 0)");
+    }
+    // input & output paths for expression level filter
+    self.sampleGroupPath = exportResults.stdoutPath;
+    self.filteredByExpressionPath = path.join(workDir, "sampleGroup_with_expr_filter_applied.tsv");
+
+      return spawnCommand(python,
+        [
+          exprFilterScript,
+          "--in_file", self.sampleGroupPath,
+          "--proportion_unexpressed", "0.8",
+          "--out_file", self.filteredByExpressionPath,
+        ],
+        workDir);
+ 
+    }).then(function(exprFilterResults){
+      console.log("expression level filter ran with results", exprFilterResults);
+      // TODO, if exit code was 1, throw error.
+      // TODO print stderr for the command
+
+      
+      
+  
+      // TODO TODO
+      // Run the variance filter here and add another then block
+      // For now, just upload it with only expression level filtering
+
+      return "variance filter results go here";
+
+    }).then(Meteor.bindEnvironment(function(varResult){
+    // Create the final Blob2, then resolve the promise.
+
+    console.log("not sure what varresult is", varResult);
+
+      var associated_samplegroup = {
+        collection_name: "SampleGroups",
+        mongo_id: sample_group_id,
+      };
+
+      var createBlob2Sync = Meteor.wrapAsync(Blobs2.create);
+      // Errors from this will be thrown to the catch below
+      var results = createBlob2Sync(self.filteredByExpressionPath, associated_samplegroup, {});
+      console.log("made blob2 with results", results);
+
+
+
+
+      // Everything worked; resolve the promise
+      console.log("apply expr async finished, resolving...now"); // XXX
+      deferred.resolve();
+    },deferred.reject())).catch(function(error){ 
+      // If we got an error anywhere along the chain,
+      // fail the job
+      deferred.reject(error);
+    });
+
+  console.log("apply expr job ran. Just waiting on the deferred now. "); // XXX
+
+  // Will wait for the async code to run and either resolve or reject
+  // before completing the job
+  return deferred.promise;
 };
 
 JobClasses.ApplyExprAndVarianceFilters = ApplyExprAndVarianceFilters ;
